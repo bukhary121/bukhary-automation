6381e8932b0157a857ec594f213a618e4461513f
bukhary121 <152088244+bukhary121@users.noreply.github.com>
2026-07-30 23:29:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function buy(){
+  window.location.href = "https://wa.me/201234567890";
+}
+
+function goProducts(){
+  window.scrollTo(0,500);
+}
+
+function login(){
+  alert("Login system coming soon");
+}
